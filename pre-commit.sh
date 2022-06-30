91454c94f36fe677ac3021b3c5c052fdb64f1a42
Matthew Greenfield <mattgreenfield1@gmail.com>
2022-06-30 10:53:55 -0700

    Adds pre-commit example

---
+++ pre-commit.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+echo "Running pre-commit hook"
+
+# If any command fails, exit immediately with that command's exit status
+set -eo pipefail
+
+# Get only changed files that match our file suffix pattern
+get_pattern_files() {
+  pattern=$(echo "$*" | sed "s/ /\$\\\|/g")
+  echo "$CHANGED_FILES" | { grep "$pattern$" || true; }
+}
+
+# Find all changed files for this commit
+# Compute the diff only once to save a small amount of time.
+CHANGED_FILES=$(git diff --name-only --cached --diff-filter=ACMR)
+
+# Get all changed python files
+SPEC_FILES=$(get_pattern_files .txt)
+
+if [[ -n "$CHANGED_FILES" ]]
+then
+  for i in "${SPEC_FILES[@]}"
+  do
+    if grep -q "fit" $i
+    then
+      echo Found \"fit\" in $i
+      exit 1
+    fi
+
+    if grep -q "fcontext" $i
+    then
+      echo Found \"fcontext\" in $i
+      exit 1
+    fi
+
+    if grep -q "fdescribe" $i
+    then
+      echo Found \"fdescribe\" in $i
+      exit 1
+    fi
+  done
+fi
