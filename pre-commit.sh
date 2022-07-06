d15454d85ec4d023a745aab440a55efd95112491
Matthew Greenfield <mattgreenfield1@gmail.com>
2022-07-06 16:20:00 -0700

    Adds condition to not fail on empty file change list

--- pre-commit.sh
+++ pre-commit.sh
@@ -5,23 +5,26 @@ echo "Running pre-commit hook"
 # If any command fails, exit immediately with that command's exit status
 set -eo pipefail
 
-git diff --name-only --cached | grep "spec.rb" | while read -r i; do
-  # because "fit" is a word, look for (fit ") or (fit ') like the beginning of a block
-  if grep -qE "fit \"|fit '" $i
-  then
-    echo Found \"fit\" in $i
-    exit 1
-  fi
+if [[ -n "$(git diff --name-only --cached | grep 'spec.rb')" ]]
+then
+  git diff --name-only --cached | grep "spec.rb" | while read -r i
+  do
+    if grep -qE "fit \"|fit '" $i
+    then
+      echo Found \"fit\" in $i
+      exit 1
+    fi
 
-  if grep -qE "fcontext" $i
-  then
-    echo Found \"fcontext\" in $i
-    exit 1
-  fi
+    if grep -qE "fcontext" $i
+    then
+      echo Found \"fcontext\" in $i
+      exit 1
+    fi
 
-  if grep -q "fdescribe" $i
-  then
-    echo Found \"fdescribe\" in $i
-    exit 1
-  fi
-done
+    if grep -qE "fdescribe" $i
+    then
+      echo Found \"fdescribe\" in $i
+      exit 1
+    fi
+  done
+fi
